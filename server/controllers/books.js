0fe8956c6d05784e573a29a0e9e54fc5f06769a4
Kiruthika Varathanathan <kvarat12@my.centennialcollege.ca>
2022-10-28 19:36:02 -0400

    logic for routes and controllers

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -16,38 +16,90 @@ export function displayBookList(req, res, next) {
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
 
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    //renders the book details page
+    res.render('index', { title: 'Add Book', page: 'books/add', book: {} })
 }
 
 // POST process the Book Details page and create a new Book - CREATE
 export function processAddPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    //processes the insertion of a new book into the database
+    let newBook = booksModel({
+        name: req.body.name,
+        author: req.body.author,
+        published: req.body.published,
+        description: req.body.description,
+        price: req.body.price
+    });
+
+    booksModel.create(newBook, (err, Book) => {
+        
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+        //redirect to list page
+        res.redirect('/books/list');
+    })
+
 }
 
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    //renders the book details page and uses the id from the URL to select the book 
+    //to document to be updated and provides an error msg
+    //uses a specific id to find the specific book
+    let id = req.params.id;
+
+    booksModel.findById(id, (err, books) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        //redners page with criteria
+        res.render('index', { title: 'Edit Book', page: 'books/edit', book: Book });
+    });
 
 }
 
 // POST - process the information passed from the details form and update the document
 export function processEditPage(req, res, next) {
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    //processes the update request of an existing book by using its id property
+    let id = req.params.id;
+
+    let newBook = booksModel({
+        _id: req.body.id,
+        name: req.body.name,
+        author: req.body.author,
+        published: req.body.published,
+        description: req.body.description,
+        price: req.body.price
+    });
+
+    booksModel.updateOne({_id: id}, newBook, (err, Book) => { 
+        if(err) {
+            console.error(err);
+            res.end(err);
+        };
+        //redirect back to list page
+        res.redirect('/books/list');
+    })
 }
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    //logic that processes the user’s delete request and removes an 
+    //existing book from database by using its id property
+    let id = req.params.id;
+
+    booksModel.remove({ _id: id }, (err) => {
+        if (err) {
+            console.error(err);
+            res.end(err);
+        }
+        //redirect back to list page
+        res.redirect('/books/list');
+    })
+
 }
