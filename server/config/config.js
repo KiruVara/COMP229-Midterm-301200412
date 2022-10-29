a6daf024f927d44d9d8526124cc291604bbbc9ff
Kiruthika Varathanathan <kvarat12@my.centennialcollege.ca>
2022-10-28 21:20:09 -0400

    Added Mongo Atlas Config

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,9 @@
-export const MongoURI = "mongodb://localhost:27017/media";
+/*File: /config/config.js
+Student Name: Kiruthika Varathanathan
+Student ID: 301200412
+Application Name: COMP229-Midterm-301200412
+Date: 2022-10-28*/
+
+//export const MongoURI = "mongodb://localhost:27017/media";
+export const MongoURI = "mongodb+srv://redMoon:qA4j5b367Vhik9St@comp229-midterm-3012004.ocr1sxq.mongodb.net/media1?retryWrites=true&w=majority";
 export const Secret = "someSecret";
